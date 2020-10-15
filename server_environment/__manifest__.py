4a5723d34ce9f6c43cc19c08222679911f018232
root <you@example.com>
2020-10-15 18:25:20 +0530

    [ADD] 'Migrated Modules'

--- server_environment/__manifest__.py
+++ server_environment/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "server configuration environment files",
-    "version": "13.0.2.3.1",
+    "version": "14.0.1.0.0",
     "depends": ["base", "base_sparse_field"],
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "summary": "move some configurations out of the database",
